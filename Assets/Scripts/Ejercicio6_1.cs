677f78148e797570aa0ea7e5db624602fe5ac684
Victendo <victormm2003@gmail.com>
2024-09-27 11:41:14 +0200

    27 / 09

---
+++ Assets/Scripts/Ejercicio6_1.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio6_1 : MonoBehaviour
+{
+    //A) (X && Y) || (X && Z) = false || true = true
+    //B) (X || !Y) && (!X || Z) = true && true = true
+    //C) X || Y && Z = X || false = true
+    //D) !(X || Y) && Z = false && Z = false
+    //E) X || Y || X && !Z && !Y = X || Y || false && !Y = X || Y || false = true || false = true
+    //F) !X || !Y || Z && X && !Y = !X || !Y || true = true || true = true
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Ejercicio6_2.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio6_2 : MonoBehaviour
+{
+    //A) W || Y && X && W || Z = W ||true && W || Z = W || true && W || Z = W || false || Z = false || Z = false
+    //B) X && !Y && !X || !W && Y = false && !X || !W && Y = true || true = true
+    //C) !(W || !Y) && X || Z = true && X || Z = true || Z = true
+    //D) X && Y && W || Z || X = true && W || Z || X = false || Z || X = false || X = true
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
